42b95d070f384379b087c3f5253b03cfcbab4c90
JeongAKo <kja2683@naver.com>
2019-04-17 17:57:41 +0900

    고정아 0417 1

--- 고정아/ContactsTableView/ContactsTableView/AddNumbersVC.swift
+++ 고정아/ContactsTableView/ContactsTableView/AddNumbersVC.swift
@@ -12,7 +12,6 @@ class AddNumbersVC: UIViewController {
 
     var xButton : UIImage = UIImage(named: "xbutton")!
     
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +20,37 @@ class AddNumbersVC: UIViewController {
         nameTextField()
         numberTextField()
         confirmButton()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
+        // 넣을때
+        
+         guard let userDatas = UserDefaults.standard.dictionary(forKey: "User") else {print("1"); return}
+        
+        
+        
+        
+        
+    }
+    func getUserData(){
+        //  꺼낼때
+        UserDefaults.standard.set(["1" : ["name": "정아", "number": "0000"] ], forKey: "User")
+        
+        guard let userDatas = UserDefaults.standard.dictionary(forKey: "User") else {print("1"); return}
+        guard let user = userDatas["1"] as? [String: String] else {print("2"); return}
+        guard let userName = user["name"], let userNumber = user["number"] else {print("3"); return}
+        print("UserName:", userName)
+        print("Number:", userNumber)
     }
     
-
+    
     
     func backButton() {
         let bButton = UIButton()
         bButton.setImage(xButton, for: UIControl.State.normal)
         bButton.frame = CGRect(x: 30, y: 60, width: 30, height: 30)
+        bButton.setTitleColor(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1), for: .normal)
         bButton.addTarget(self, action: #selector(dis(_:)), for: .touchUpInside)
         view.addSubview(bButton)
     }
@@ -38,6 +59,7 @@ class AddNumbersVC: UIViewController {
         let nameText = UITextField()
         nameText.frame = CGRect(x: view.frame.width / 2 - 150, y: 300, width: 300, height: 40)
         nameText.backgroundColor = .white
+        
         
         view.addSubview(nameText)
     }
--- 고정아/ContactsTableView/ContactsTableView/DetailContactsVC.swift
+++ 고정아/ContactsTableView/ContactsTableView/DetailContactsVC.swift
@@ -14,6 +14,8 @@ class DetailContactsVC: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         // Do any additional setup after loading the view.
+        
+        
     }
     
     
