73083052beb4865aa577c05bea159825fd410212
hyeoktae kwon <kht_0218@hanmail.net>
2019-04-18 16:27:45 +0900

    권혁태 - 20190418

---
+++ 권혁태/StoryboardDesignableExample - Starter/StoryboardDesignableExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  StoryboardDesignableExample
+//
+//  Created by 이봉원 on 12/04/2019.
+//  Copyright © 2019 giftbot. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var customButton: CustomButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViewConstraints()
+//        print(customButton.someValue)
+    }
+    
+}
+
